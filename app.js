a9e78c4e4d8e3cfb7895686aca300ca13d6b62ec
Manuel Alfaro <manuel.alfaro1488@gmail.com>
2022-04-25 20:34:37 -0500

    ejemplo terminado

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const express = require('express')
 
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{
+    res.send('Hola mundo')
+})
+
+app.get('/launchx', (req, res) =>{
+    res.send('LaunchX page')
+})
+
+app.get('/explorersInNode', (req, res) =>{
+    const explorer = { name: "Explorer", msg: "Hello" }
+    res.send(explorer)
+})
+
+
+app.get('/explorers/:explorerName', (req, res) =>{    
+    res.send(req.params)
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`App listening on port ${port}`)
+})
